402cccc0ba2d8ff140ba579a2499d4d87ae5742c
ggarvit21 <100669396+ggarvit21@users.noreply.github.com>
2024-10-10 21:32:33 +0530

    Debugging

--- create.py
+++ create.py
@@ -110,6 +110,5 @@ os.system(f"g++ makemonotone.cpp  -L{s} -lgmp")
 
 result = os.system(f"g++ makemonotone.cpp -L{s} -lgmp > /dev/null 2>&1")
 
-# Check if the compilation failed
 if result != 0:
     print("Error: Compilation failed.")
